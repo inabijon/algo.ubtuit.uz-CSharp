f902277e5afa826bbd1d4f450dcf78b914f83afc
NABIJON <nabijonibodullaev@gmail.com>
2023-03-07 15:36:57 +0500

    1

---
+++ 1/Program.cs
@@ -0,0 +1,11 @@
+﻿//Find the volume of a cube of dimension x.
+
+double x = 0.0;
+
+Console.WriteLine("Enter a number: ");
+
+x = Convert.ToDouble(Console.ReadLine());
+
+double y = Math.Pow(x, 3);
+
+Console.WriteLine("The cube of " + x + " is " + y);
