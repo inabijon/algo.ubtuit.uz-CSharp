f6f4d3834f135c6d58a32d76b09e5ebb2938749c
NABIJON <nabijonibodullaev@gmail.com>
2023-03-07 18:34:57 +0500

    11

---
+++ 11/Program.cs
@@ -0,0 +1,19 @@
+﻿int n = 0;
+
+Console.Write("Please enter n: ");
+n = Convert.ToInt32(Console.ReadLine());
+
+if(n < 0) {
+    Console.WriteLine("Result is - " + 1);
+}
+
+int sum = 0;
+
+for(int i = 1; i<=n; i++) {
+    sum += i;
+}
+
+Console.WriteLine("Total sum is " + sum);
+
+Console.WriteLine("Press any key");
+Console.ReadKey();
