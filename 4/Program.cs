198e380a8612725ad0d2e1939b13da7df563a4e9
NABIJON <nabijonibodullaev@gmail.com>
2023-03-07 16:33:19 +0500

    4

---
+++ 4/Program.cs
@@ -0,0 +1,19 @@
+﻿int r = 0;
+
+Console.Write("Enter the number of radius: ");
+r = Convert.ToInt32(Console.ReadLine());
+
+if(r < 0)
+{
+    Console.WriteLine("Invalid input");
+    Console.WriteLine("Press any key to exit");
+    Console.ReadKey();
+    return;
+}
+
+double area = Math.Round(4 * Math.PI * r * r, 2);
+
+Console.WriteLine("The area of the sphere is: " + area);
+
+Console.WriteLine("Press any key to exit");
+Console.ReadKey();
