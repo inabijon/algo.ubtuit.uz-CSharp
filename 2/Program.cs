df50b3c88f0898335581e53a9891989dbba98e27
NABIJON <nabijonibodullaev@gmail.com>
2023-03-07 16:07:51 +0500

    second way added

--- 2/Program.cs
+++ 2/Program.cs
@@ -17,17 +17,18 @@ if(r1 < 0 || r2 < 0 || r3 < 0)
     return;
 }
 
-double s1 = r1 * r1 * Math.PI;
-double s2 = r2 * r2 * Math.PI;
-double s3 = r3 * r3 * Math.PI;
+double s1 = Math.Round(r1 * r1 * Math.PI, 2);
+double s2 = Math.Round(r2 * r2 * Math.PI, 2);
+double s3 = Math.Round(r3 * r3 * Math.PI, 2);
 
-string result1 = s1.ToString("0.00");
-string result2 = s2.ToString("0.00");
-string result3 = s3.ToString("0.00");
+// Second way to do it
+// string result1 = s1.ToString("0.00");
+// string result2 = s2.ToString("0.00");
+// string result3 = s3.ToString("0.00");
 
-Console.WriteLine("The first resistor area is: " + result1);
-Console.WriteLine("The second resistor area is: " + result2);
-Console.WriteLine("The third resistor area is: " + result3);
+Console.WriteLine("The first resistor area is: " + s1);
+Console.WriteLine("The second resistor area is: " + s2);
+Console.WriteLine("The third resistor area is: " + s3);
     
 Console.WriteLine("Press any key to exit");
 Console.ReadKey();
